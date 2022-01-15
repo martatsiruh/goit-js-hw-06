17ae6cebaf2eb38b8c9043ca1067ab099e16818a
Marta Tsiruh <martatsiruch@gmail.com>
2022-01-15 12:53:41 +0100

    add Task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+
+const bodyEl = document.querySelector('body');
+//console.log(bodyEl);
+const colorChange = document.querySelector('.color');
+//console.log(colorChange);
+const buttonChangeColor = document.querySelector('.change-color');
+//console.log(buttonChangeColor);
+
+buttonChangeColor.addEventListener('click', (event) => {
+  const palletColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = palletColor;
+  //console.log(palletColor);
+  colorChange.textContent = palletColor;
+  //console.log(colorChange)
+});
