8d789c48b1f04404e383d57ad4b8a7aee0ec7bc3
Marta Tsiruh <martatsiruch@gmail.com>
2022-01-16 09:18:16 +0100

    correct task 8

--- js/task-08.js
+++ js/task-08.js
@@ -6,12 +6,17 @@ formRef.addEventListener("submit", onFormSubmit);
 function onFormSubmit(event) {
     event.preventDefault();
 
+    let formData = {};
     const { elements: { email, password } } = event.currentTarget;
     
-    if (email.value === '' || password === '') {
-        return console.error("Please fill all the free fields!")
+    if (email.value === '' || password.value === '') {
+        return alert("Please fill all the free fields!");
     };
 
-    console.log(`Login: ${email.value}, Password ${password.value}`);
+    formData.email = `$(email.value)`;
+    formData.password = `$(password.value)`;
+    console.log(formData);
+
+    //console.log(`Login: ${email.value}, Password ${password.value}`);
     event.currentTarget.reset();
 }
